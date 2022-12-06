19088194fc63b590526a7f78b61ff7970ac447a3
Ahmad Murad Malik <ahmad_murad@berkeley.edu>
2022-12-05 18:46:48 -0800

    idk

--- src/index.js
+++ src/index.js
@@ -3,23 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Login from './Components/Login.js';
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/Login",
-    element: <Login/>,
-  }
-]);
 
 ReactDOM.render(
   <React.StrictMode>
